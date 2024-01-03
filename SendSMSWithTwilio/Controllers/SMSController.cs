7720070c30390ed66744b57600e36a7f41a69b8c
dell <dell@DESKTOP-C48NBE9>
2024-01-03 12:44:57 -0800

    Add project files.

---
+++ SendSMSWithTwilio/Controllers/SMSController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SendSMSWithTwilio.DTOs;
+using SendSMSWithTwilio.Services;
+
+namespace SendSMSWithTwilio.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SMSController : ControllerBase
+    {
+        private readonly ISMSService _smsService;
+
+        public SMSController(ISMSService smsService)
+        {
+            _smsService = smsService;
+        }
+        [HttpPost("send")]
+        public IActionResult Send(SMSDto dto)
+        {
+            var result = _smsService.Send(dto.PhoneNumber, dto.Body);
+
+            if(!string.IsNullOrEmpty(result.ErrorMessage)) 
+                return BadRequest(result.ErrorMessage);
+            return Ok(result);
+
+        }
+    }
+}
---
+++ SendSMSWithTwilio/DTOs/SMSDto.cs
@@ -0,0 +1,8 @@
+﻿namespace SendSMSWithTwilio.DTOs
+{
+    public class SMSDto
+    {
+        public string PhoneNumber { get; set; }
+        public string Body { get; set; }
+    }
+}
